979efdb347362cbbeca7cbedbcf8d0f51bbde807
MusH <collinsmucheru@gmail.com>
2020-01-31 20:24:31 +0300

    delayed non-blocking fibonacci recursion - return after 3 seconds

--- lib/fibonacci_sequence.ex
+++ lib/fibonacci_sequence.ex
@@ -3,14 +3,33 @@ defmodule FibonacciSequence do
   Documentation for FibonacciSequence
   """
   @spec recursion(integer()) :: integer()
-  def recursion(0), do: 0
-  def recursion(1), do: 1
-  def recursion(int), do: recursion(0, 1, int-2)
+  def recursion(0) do
+    IO.inspect(0, label: "recursion")
+    0
+  end
+  def recursion(1) do
+    IO.inspect(1, label: "recursion")
+    1
+  end
+
+  def recursion(int) do
+    res = recursion(0, 1, int-2)
+    IO.inspect(res, label: "recursion")
+    res
+  end
 
   #base case
-  def recursion(_, int, -1), do: int
+  def recursion(_, int, -1) do
+    IO.inspect(int, label: "recursion")
+    int
+  end
   def recursion(prev_int, int, n) do
     next = prev_int + int
+    IO.inspect(next, label: "recursion")
     recursion(int, next, n-1)
+  end
+
+  def delayed_non_blocking_recursion(int) do
+    spawn(fn -> :timer.sleep(3000); recursion(int) end)
   end
 end
